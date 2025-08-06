395bc17ddecb7bd8f484c127bf9655c8993f3ae2
xaioc1925 <xcssx1925@gmail.com>
2025-08-06 13:46:40 +0800

    Initial Commit

---
+++ BRM/Hello.cs
@@ -0,0 +1,5 @@
+class Program{
+	static void Main(string[] args){
+		System.Console.Write(Newtonsoft.Json.JsonConvert.SerializeObject("Hello 老肖 又来了"));
+	}
+}
---
+++ BuildAndVue/Program.cs
@@ -0,0 +1,29 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+class Programe
+{
+    static void Main(string[] args)
+    {
+        System.Console.Write("111");
+    }
+}
